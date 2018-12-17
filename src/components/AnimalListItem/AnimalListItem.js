1d19efb6a8b14ffde5a73c64a42859a0fd3ab0f7
Travis Lang <tlang50@icloud.com>
2018-12-17 14:04:23 -0600

    add delete route and logic to remove animals

--- src/components/AnimalListItem/AnimalListItem.js
+++ src/components/AnimalListItem/AnimalListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE!
 
+
+
 class AnimalListItem extends Component {
+
+    //dispatch to saga to handle async delete from db
+    handleDelete = id => e => {
+        this.props.dispatch({type: 'DELETE_ANIMAL', payload: id})
+    }
     // Renders the list of animals
     render() {
         return (
             <tr>
                 <td>{this.props.classData.species_name}</td>
                 <td>{this.props.classData.class_name}</td>
+                <td><button onClick={this.handleDelete(this.props.classData.id)}>Remove From List</button></td>
             </tr>
         );
     }
 }
 
-export default AnimalListItem;
+export default connect()(AnimalListItem);
