6f00e2266cac7b524cd142806b1264b5b15a4823
Travis Lang <tlang50@icloud.com>
2018-12-17 13:16:38 -0600

    add GET route and fetchAnimals saga. V1.0

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
+    pool.query(`SELECT "species".*, "class".class_name from "species"
+    LEFT OUTER JOIN "class" ON "class".id = "species".class_id
+    ORDER BY "species".id ASC;
+    `).then( result => {
+        res.send(result.rows);
+    }).catch( err => {
+        console.log('error in GET DB query:', err);
+        res.sendStatus(500);
+    })
     
 });
 
--- src/index.js
+++ src/index.js
@@ -9,10 +9,17 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+function* fetchAnimals() {
+    const zooAnimals = yield call(axios.get, '/zoo');
+    yield put({type: 'SET_ZOO_ANIMALS', payload: zooAnimals.data})
+}
 
 // Your saga should listen for the action type of `GET_ZOO_ANIMALS`
 function* rootSaga() {
-    // YOUR CODE HERE
+    yield takeEvery('GET_ZOO_ANIMALS', fetchAnimals)
 
 }
 
